875259bfb62ce9db78dadc9d447d8fe601fd6891
lau1406 <lkeijzer@live.nl>
2017-03-20 10:23:40 +0100

    - added global vars - added method to fill fields var

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,29 @@
+// Global variables
+var lengthSquare = 0;   // Length of side of square dirt
+var fields = [];       // List of squares that the farmer has
+
+// Square is a piece of land
+var square = {
+    'used': false,
+    'nitrate': 0
+};
+
+
+var genField = function (x, y) {
+    if (typeof x != 'number' || typeof y != 'number') {
+        throw new TypeError("x or y isn't a number");
+    }
+    for (var i = 0; i < x; i++) {
+        fields[i] = [];
+        for (var j = 0; j < y; j++) {
+            fields[i][j] = square;
+        }
+    }
+    fields[1][1].used = true;
+};
+
+
 function startModel() {
-  window.alert("potatoe OK");
+    // TODO: change 2,2 to what the farmer fills in
+    genField(2, 2);
 }
